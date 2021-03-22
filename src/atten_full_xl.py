3962e4d67f8621328fa43ce6948cca95754bf85c
nantha42 <krnantha13@gmail.com>
2021-03-22 19:09:11 +0530

    modified fullxl model to workable

--- src/atten_full_xl.py
+++ src/atten_full_xl.py
@@ -127,6 +127,7 @@ class MultiheadAttentionXL(nn.Module):
             attn.pop_last(n)
 
 
+
 class AttenModFullXL(torch.nn.Module):
     def __init__(self):
         super().__init__()
@@ -142,32 +143,43 @@ class AttenModFullXL(torch.nn.Module):
         self.features = None
         self.memory = []
         dicts = {}
+
         for i in range(16):
-            dicts["M-atten-" + str(i)] = MultiheadAttentionXL(64,16, 4, dp=0.2)     
+            dicts["M-atten-" + str(i)] = MultiheadAttentionXL(64,16, 4, dp=0.2)
         
         self.multiheads = nn.ModuleDict(dicts)
         self.atten_dropout = nn.Dropout(0.2)
-        
         self.layernorm1 = nn.LayerNorm(64)
-
-
 
         num_layers = 2
         self.xl_heads = {}
         self.norm_layers = {}
+        self.feedforward = {}
+        self.feed_norm_layers = {}
+
         for i in range(num_layers):
             self.xl_heads["XL-"+str(i)] = MultiheadAttentionXL(32*64,256,8,dp=0.2)
         self.xl_heads = nn.ModuleDict(self.xl_heads) 
 
         for i in range(num_layers):
             self.norm_layers["LN-"+str(i)] = nn.LayerNorm(32*64) 
-        self.norm_layers = nn.ModuleDict(self.norm_layers)         
+        self.norm_layers = nn.ModuleDict(self.norm_layers)
 
+        for i in range(num_layers):
+            self.feedforward["FN-"+str(i)] = nn.Linear(32*64,32*64)
+        self.feedforward = nn.ModuleDict(self.feedforward)
+
+        for i in range(num_layers):
+            self.feed_norm_layers["LN-"+str(i)] = nn.LayerNorm(32*64) 
+        self.feed_norm_layers = nn.ModuleDict(self.feed_norm_layers)         
         
         self.flin1 = nn.Linear(16*2048,512)
         self.flin2 = nn.Linear(512,128)
         self.flin3 = nn.Linear(128,25)
         self.att_mat = None
+
+        if torch.cuda.is_available():
+            self = self.cuda()
 
     def pop_memory(self,n):
         for i in range(len(self.multiheads)):
@@ -177,16 +189,21 @@ class AttenModFullXL(torch.nn.Module):
             self.xl_heads["XL-"+str(i)].pop_last(n)
 
     def forward(self,t,discard_mem = False):
+
         t = t/255.0 # normaliztion added to the model forward itself
         bs = t.shape[0]
-        sliced = torch.zeros((t.shape[0],16, 32, 32))
-        # ic(x.shape)
+        
+        if torch.cuda.is_available():
+            sliced = torch.zeros((t.shape[0],16, 32, 32)).cuda()
+        else:
+            sliced = torch.zeros((t.shape[0],16, 32, 32))
+
         for i in range(4):
             for j in range(4):
                 sliced[:,i * 4 + j] = t[:,i * 32:i * 32 + 32, j * 32:j * 32 + 32]
 
         x = sliced
-        x = x.reshape((bs*16, 1, 32, 32))
+        x = x.view((bs*16, 1, 32, 32))
         
         u = self.conv1(x)
         u = self.pool1(u)
@@ -196,41 +213,51 @@ class AttenModFullXL(torch.nn.Module):
         u = self.conv2(u)
         u = self.pool2(u)
         u = self.dropout2(u)
-        u = u.reshape((bs*16,32,36))
-        u = self.expand(u)         
+
+
+        u = u.view((bs,16,32,36))
+        u = self.expand(u)
         u = self.relu1(u)
 
-        # ic(u.shape)
-        u = u.reshape((16,bs,32,64))
+        # u = u.reshape((bs,16,32,64))
         attention_out =[]
         self.att_mat = []
+
+        
         for i in range(len(self.multiheads)):
-            att_out, _att_mat = (self.multiheads["M-atten-" + str(i)](u[i] ,u[i], u[i],discard_mem))
+            tu = u[:,i]
+            tu = tu.view(bs,32,64)
+            att_out, _att_mat = (self.multiheads["M-atten-" + str(i)](tu ,tu, tu,discard_mem))
             self.att_mat.append(_att_mat.detach().clone())
+            att_out = torch.unsqueeze(att_out,1)
             attention_out.append(att_out)
             
         with torch.no_grad():
             self.att_mat = torch.stack(self.att_mat)
-
-        attention_out_stacked = torch.stack(attention_out) 
+        
+        attention_out_stacked = attention_out[0]
+        
+        for i in range(1,16):
+            attention_out_stacked = torch.cat( (attention_out_stacked,attention_out[i]),1)
         
         u = u + self.atten_dropout(attention_out_stacked)
         u = self.layernorm1(u)
-        u = u.reshape((16,bs,32*64))
-        u = u.transpose(0,1) 
+        
 
+        u = u.view((bs,16,32*64))
+        
         for i in range(len(self.xl_heads.keys())):
             att_out,_att_mat = self.xl_heads["XL-"+str(i)](u,u,u,discard_mem)
             u = u+ torch.nn.functional.dropout(u)
-            u = self.norm_layers["LN-"+str(i)]
+            u = self.norm_layers["LN-"+str(i)](u)
+            u = u + self.feedforward["FN-"+str(i)](u)
+            u = self.feed_norm_layers["LN-"+str(i)](u)
         
-        u = u.reshape((bs,16*2048))
+        
+        u = u.view((bs,16*2048))
         u = self.flin1(u)
         u = nn.functional.relu(u)
         u = self.flin2(u)
         u = nn.functional.relu(u)
         u = self.flin3(u)
-        # ic(u.shape)
         return u
-
-
